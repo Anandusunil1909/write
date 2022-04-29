a30fc22078d5f8425097a9532cc8ed1171f7c863
Anandusunil1909 <anandusunil1909@gmail.com>
2022-04-29 10:27:09 +0530

    Added my project

---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+//scribble is being used : reference to the same  at :https://pub.dev/packages/scribble
+//the page to be integrated to main file
+//next button to be added , save and next  button to be integrated to the same
+//changes to be made according to ui
+// to be tested for working with s pen
+// stroke size to be altered while testing
+
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:scribble/scribble.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scribble',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'share your thoughts'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScribbleNotifier notifier;
+
+  @override
+  void initState() {
+    notifier = ScribbleNotifier();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(Icons.save),
+          tooltip: "Save to Image",
+          onPressed: () => _saveImage(
+              context), // save image icon on app bar to be moved accordingley
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Scribble(
+                notifier: notifier,
+                drawPen: true,
+              ),
+              Positioned(
+                top: 16,
+                right: 16,
+                child: Column(
+                  children: [
+                    _buildColorToolbar(context),
+                    const Divider(
+                      height: 32,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // image save button function ,image saved in buffer to be dircted to respective file, also image shown in pop up value which is not needed
+  Future<void> _saveImage(BuildContext context) async {
+    final image = await notifier.renderImage();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(""),
+        content: Image.memory(image.buffer.asUint8List()),
+      ),
+    );
+  }
+
+  // the whole tool bar to be removed and aligned as per the ui
+  Widget _buildColorToolbar(BuildContext context) {
+    return StateNotifierBuilder<ScribbleState>(
+      stateNotifier: notifier,
+      builder: (context, state, _) => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _buildUndoButton(context),
+          const Divider(
+            height: 4.0,
+          ),
+          _buildClearButton(context),
+          const Divider(
+            height: 20.0,
+          ),
+          _buildEraserButton(context, isSelected: state is Erasing),
+          _buildColorButton(context, color: Colors.black, state: state),
+        ],
+      ),
+    );
+  }
+
+  //eraser code
+  Widget _buildEraserButton(BuildContext context, {required bool isSelected}) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: FloatingActionButton.small(
+        tooltip: "Erase",
+        backgroundColor: const Color(0xFFF7FBFF),
+        elevation: isSelected ? 10 : 2,
+        shape: !isSelected
+            ? const CircleBorder()
+            : RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+        child: const Icon(Icons.remove, color: Colors.blueGrey),
+        onPressed: notifier.setEraser,
+      ),
+    );
+  }
+
+  //colour button code
+  Widget _buildColorButton(
+    BuildContext context, {
+    required Color color,
+    required ScribbleState state,
+  }) {
+    final isSelected = state is Drawing && state.selectedColor == color.value;
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: FloatingActionButton.small(
+          backgroundColor: color,
+          elevation: isSelected ? 10 : 2,
+          shape: !isSelected
+              ? const CircleBorder()
+              : RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+          child: Container(),
+          onPressed: () => notifier.setColor(color)),
+    );
+  }
+
+  //undo button code
+  Widget _buildUndoButton(
+    BuildContext context,
+  ) {
+    return FloatingActionButton.small(
+      tooltip: "Undo",
+      onPressed: notifier.canUndo ? notifier.undo : null,
+      disabledElevation: 0,
+      backgroundColor: notifier.canUndo ? Colors.blueGrey : Colors.grey,
+      child: const Icon(
+        Icons.undo_rounded,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  //clear button code
+
+  Widget _buildClearButton(BuildContext context) {
+    return FloatingActionButton.small(
+      tooltip: "Clear",
+      onPressed: notifier.clear,
+      disabledElevation: 0,
+      backgroundColor: Colors.blueGrey,
+      child: const Icon(Icons.clear),
+    );
+  }
+}
